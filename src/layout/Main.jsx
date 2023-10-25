ffbb853f1202db418c27b4a4b2445485f4aee37f
milordplus <milordplus@mail.ru>
2023-10-25 21:01:36 +0300

    02

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,35 @@
-function Main(){
-    return <main className="container content">
-        Hello from Movies App
-    </main>
+import React from "react";
+import {Movies} from "../components/Movies";
+import {Preloader} from "../components/Preloader";
+import {Search} from "../components/Search";
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=eaef17d&s=terminator')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    searchMovies = (str, type = 'all') => {
+        fetch(`http://www.omdbapi.com/?apikey=eaef17d&s=${str}${type !== "all" ? `&type=${type}` : ""}`)
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+        const {movies} = this.state;
+
+        return <main className='container content'>
+            <Search searchMovies={this.searchMovies}/>
+            {
+                movies.length ? (<Movies movies={this.state.movies}/>) : <Preloader/>
+            }
+        </main>
+    }
 }
 
 export {Main}
